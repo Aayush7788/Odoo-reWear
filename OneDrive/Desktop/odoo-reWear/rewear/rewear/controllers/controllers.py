44dba7cf893b8a213f08684629dfb25cc37a0831
Aayush7788 <aayushkotadia1234@outlook.com>
2025-07-12 12:13:48 +0530

    Add website controllers for landing, browse, item detail, add item, and user dashboard

--- OneDrive/Desktop/odoo-reWear/rewear/rewear/controllers/controllers.py
+++ OneDrive/Desktop/odoo-reWear/rewear/rewear/controllers/controllers.py
@@ -1,21 +1,51 @@
-# from odoo import http
+from odoo import http
+from odoo.http import request
 
+class RewearWebsite(http.Controller):
+    @http.route('/', type='http', auth='public', website=True)
+    def landing(self, **kw):
+        featured_items = request.env['rewear.item'].search([('availability', '=', 'available')], limit=5)
+        return request.render('rewear.landing', {'featured_items': featured_items})
 
-# class Rewear(http.Controller):
-#     @http.route('/rewear/rewear', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+    @http.route('/rewear/browse', type='http', auth='public', website=True)
+    def browse(self, **kw):
+        items = request.env['rewear.item'].search([('availability', '=', 'available')])
+        return request.render('rewear.browse', {'items': items})
 
-#     @http.route('/rewear/rewear/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('rewear.listing', {
-#             'root': '/rewear/rewear',
-#             'objects': http.request.env['rewear.rewear'].search([]),
-#         })
+    @http.route('/rewear/item/<int:item_id>', type='http', auth='public', website=True)
+    def item_detail(self, item_id, **kw):
+        item = request.env['rewear.item'].sudo().browse(item_id)
+        return request.render('rewear.item_detail', {'item': item})
 
-#     @http.route('/rewear/rewear/objects/<model("rewear.rewear"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('rewear.object', {
-#             'object': obj
-#         })
+    @http.route('/rewear/add', type='http', auth='user', website=True, methods=['GET', 'POST'])
+    def add_item(self, **post):
+        if request.httprequest.method == 'POST':
+            vals = {
+                'title': post.get('title'),
+                'description': post.get('description'),
+                'category': post.get('category'),
+                'type': post.get('type'),
+                'size': post.get('size'),
+                'condition': post.get('condition'),
+                'tags': post.get('tags'),
+                'image': post.get('image'),
+                'points_cost': int(post.get('points_cost', 10)),
+                'owner_id': request.env.user.id,
+            }
+            request.env['rewear.item'].sudo().create(vals)
+            # Award points to user (logic can be improved)
+            request.env.user.sudo().points_balance += 10
+            return request.redirect('/my/dashboard')
+        return request.render('rewear.add_item', {})
+
+    @http.route('/my/dashboard', type='http', auth='user', website=True)
+    def user_dashboard(self, **kw):
+        user = request.env.user
+        my_items = request.env['rewear.item'].search([('owner_id', '=', user.id)])
+        my_swaps = request.env['rewear.swap'].search([('requester_id', '=', user.id)])
+        return request.render('rewear.user_dashboard', {
+            'user': user,
+            'my_items': my_items,
+            'my_swaps': my_swaps,
+        })
 
